442e0f3ef322c8d36427391ff5b9db4390dbb38f
Stefan Katzensteiner <s.katzensteiner@gmail.org>
2023-09-01 10:43:15 +0200

    Implemented FlexibleGreedy

---
+++ src/main/java/io/gocklkatz/daypacker/DpAlgorithmFullEnumeration.java
@@ -0,0 +1,11 @@
+package io.gocklkatz.daypacker;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class DpAlgorithmFullEnumeration implements DpAlgorithm {
+    @Override
+    public Result solve(List<Food> foods, int maxCost, Comparator<Food> comp) {
+        return new Result();
+    }
+}
